dd61c0d4d9175541fde156a8097f3b42ff1e0f65
JCKMTU <jckoole@mtu.edu>
2017-05-29 10:41:52 +0800

    updated

--- single-perceptron.py
+++ single-perceptron.py
@@ -1,25 +1,35 @@
 import numpy as np
 
-# input_dir
+input_dir = [{ 'x': 0, 'y': 1, 'z': 1}, { 'x': 1, 'y': 2, 'z': 2}]
 # output_dir
 
 # Shapekeys and blender position
-shapekeys = []
-blender_pos = []
+shapekey_list = []
+
+for _in in input_dir:
+    tmp_list = []
+    for _cord in _in:
+        #tmp_list.append(_in[_cord])
+        shapekey_list.append(_in[_cord])
+    #shapekey_list.append(tmp_list)
+print(shapekey_list)
 
 # Weights and biases with random values in normal distribution
 mean, variance = 0, 1
-weights = np.random.normal(loc=mean, scale=np.sqrt(variance), size=20)
-biases = np.random.normal(loc=mean, scale=np.sqrt(variance), size=20)
+weights = np.random.normal(loc=mean, scale=np.sqrt(variance), size=len(shapekey_list))
+biases = np.random.normal(loc=mean, scale=np.sqrt(variance), size=len(shapekey_list))
 
 # Calculate sum(shapekey_i * weights_i) + bias
-output = np.add(np.matmul(shapekeys, weights), biases)
+output = np.add(np.matmul(shapekey_list, weights), biases)
 
 # Apply activation function
-output = np.relu(output)
+#output = np.relu(output)
+output = output * (output > 0)
 
 # Sum of array in given axis
-#output = np.sum(output)
+output = np.sum(output)
 
 # Fitness function
-fitness = np.sqrt(np.pow(shapekeys - blender_pos, 2))
+#fitness = np.sqrt(np.pow(shapekeys - blender_pos, 2))
+
+print output
