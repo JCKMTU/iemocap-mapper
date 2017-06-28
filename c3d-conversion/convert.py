5fdabd89344cdac19f3ae07ebb334df50d9da2b8
elggem <trilly@gmail.com>
2017-06-28 21:29:45 +0800

    finally cleanup convert script...

--- c3d-conversion/convert.py
+++ c3d-conversion/convert.py
@@ -1,36 +1,33 @@
 import c3d
 import numpy as np
 
+input_filename = sys.argv[1]
 
-"""
-set point rate (1200)
-
-
-"""
-line = open('data.csv').readline()
+# Get the labels from the first line in the file
+line = open(input_filename).readline()
 labels = line[:-2].split(" ")[2:]
 
-plonk = []
+# Left pad them for C3D writer
+aligned = []
+for label in labels:
+    aligned.append(label.ljust(8))
+labels = aligned
 
-for hu in labels:
-    plonk.append(hu.ljust(8))
+# Load the data
+data = np.genfromtxt(input_filename, skip_header=2)
+# Invert all coordinates...
+data *= -1
 
-labels = plonk
+# Pad with two zero columns per point for error and nr_cams
+reshaped_data = data[:,2:].reshape([-1,55,3])
+padding_data = np.zeros([foo.shape[0],55,2])
 
-data = np.genfromtxt("./data.csv", skip_header=2)
+points = np.concatenate([reshaped_data,padding_data], axis=2).reshape([reshaped_data.shape[0],-1])
 
-data = 1 - data
-
-foo = data[:,2:].reshape([-1,55,3])
-bar = np.zeros([foo.shape[0],55,2])
-
-yolo = np.concatenate([foo,bar], axis=2)
-
-swag = yolo.reshape([foo.shape[0],-1])
-
+# Write the file
 writer = c3d.Writer()
 
-for frame in yolo:
+for frame in points:
     writer.add_frames([frame])
 
 with open('conv-points.c3d', 'wb') as h:
