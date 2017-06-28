a989edbaf355448df0956cce619292959e148b7b
elggem <trilly@gmail.com>
2017-06-28 21:57:32 +0800

    bugfix conversion script

--- c3d-conversion/iemocap2c3d.py
+++ c3d-conversion/iemocap2c3d.py
@@ -1,7 +1,9 @@
+import sys
 import c3d
 import numpy as np
 
 input_filename = sys.argv[1]
+output_filename = input_filename.split(".")[1] + ".c3d"
 
 # Get the labels from the first line in the file
 line = open(input_filename).readline()
@@ -16,13 +18,12 @@ labels = aligned
 # Load the data
 data = np.genfromtxt(input_filename, skip_header=2)
 # Invert all coordinates...
-data *= -1
+data = 1 - data
 
 # Pad with two zero columns per point for error and nr_cams
 reshaped_data = data[:,2:].reshape([-1,55,3])
-padding_data = np.zeros([foo.shape[0],55,2])
-
-points = np.concatenate([reshaped_data,padding_data], axis=2).reshape([reshaped_data.shape[0],-1])
+padding_data = np.zeros([reshaped_data.shape[0],55,2])
+points = np.concatenate([reshaped_data,padding_data], axis=2)
 
 # Write the file
 writer = c3d.Writer()
@@ -30,5 +31,5 @@ writer = c3d.Writer()
 for frame in points:
     writer.add_frames([frame])
 
-with open('conv-points.c3d', 'wb') as h:
+with open(output_filename, 'wb') as h:
     writer.write(h, labels)
